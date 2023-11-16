014581a0e991ab6f157db312f0b4b319b3e6d7e6
Claude White <claude.white2005@gmail.com>
2023-11-16 14:04:41 -0500

    First Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className='grid w-screen h-screen place-items-center'>
+        <span className='text-6xl font-bold text-transparent bg-[linear-gradient(120deg,#bd34fe,30%,#41d1ff)] bg-clip-text'>
+          Vite + Tailwind
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
